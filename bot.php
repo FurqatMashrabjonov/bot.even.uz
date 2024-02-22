e66180c23d179e6ab1736774cf9d40c0fb31a37e
Furqatbek Mashrabjonov <61729454+FurqatMashrabjonov@users.noreply.github.com>
2024-02-22 18:48:07 +0500

    catch errors

--- bot.php
+++ bot.php
@@ -1,7 +1,11 @@
 <?php
 require_once './vendor/autoload.php';
+try {
 
-$config = require 'config/telegram.php';
+    $config = require 'config/telegram.php';
 
-require_once './bootstrap/bootstrap.php';
+    require_once './bootstrap/bootstrap.php';
 
+}catch (Exception $e){
+    echo $e->getMessage();
+}
